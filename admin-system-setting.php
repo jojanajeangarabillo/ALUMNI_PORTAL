4997ef09ebe3eb49f763dc1af99daeac18439b13
Jojana jean Garabillo <jojanajeangarabillo@gmail.com>
2025-04-28 21:59:17 +0800

    fixed

--- admin-system-setting.php
+++ admin-system-setting.php
@@ -28,15 +28,16 @@
             </div>
 
             <a href="admin-home.php"><img src="images/home.png" alt="Home"><span>Home</span></a>
-            <a href="admin-gallery.php" class="active"><img src="images/gallery.png" alt="Gallery"><span>Gallery</span></a>
+            <a href="admin-gallery.php"><img src="images/gallery.png" alt="Gallery"><span>Gallery</span></a>
             <a href="#"><img src="images/course-list.png" alt="Course List"><span>Course List</span></a>
             <a href="admin-alumni-list.php"><img src="images/alumni_list.png" alt="Alumni List"><span>Alumni List</span></a>
             <a href="#"><img src="images/upload.png" alt="Alumni Upload"><span>Alumni Upload</span></a>
             <a href="#"><img src="images/jobs.png" alt="Jobs"><span>Jobs</span></a>
             <a href="register.php"><img src="images/calendar.png" alt="Events"><span>Events</span></a>
             <a href="landing.php"><img src="images/forums.png" alt="Forum"><span>Forum</span></a>
+            <a href="admin-system-setting.php" class="active"><img src="images/settings.png" alt="System Settings"><span>System Settings</span></a>
             <a href="landing.php"><img src="images/log-out.png" alt="Log Out"><span>Log Out</span></a>
-            <a href="landing.php"><img src="images/settings.png" alt="System Settings"><span>System Settings</span></a>
+           
         </div>
     </div>
 
