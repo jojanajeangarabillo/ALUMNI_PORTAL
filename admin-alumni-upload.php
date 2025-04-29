c40d0f6c73241386237a223a18f5a2d0cb1aa4e5
Rhoanne Nicole Antonio <antonio_rhoannenicole@plpasig.edu.ph>
2025-04-29 09:51:27 +0800

    upload page for admin

---
+++ admin-alumni-upload.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Alumni Portal</title>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <div class="interface-header">
+            <img src="images/logo.png" alt="PLP Logo" class="logo-interface">
+            <div class="text">
+                <div class="school-name">Pamantasan Ng Lungsod Ng Pasig</div>
+                <p class="p-size">Alkade Jose St. Kapasigan, Pasig City</p>
+            </div>
+        </div>
+
+        <div class="upload">
+            <div class="manage-profile">
+                <div class="sidebar" id="sidebar">
+                    <div class="toggle-btn" onclick="toggleSidebar()">&#x25C0;</div>
+
+                    <div class="sidebar-content">
+                        <div class="profile-section">
+                                <a href="manage-profile.php" class="profile-pic">
+                                <img src="images/avatar.png" alt="Profile Picture">
+                                </a>
+                            <div class="profile-name">Marian</div>
+                        </div>
+
+                        <a href="admin-home.php" class="active"><img src="images/home.png" alt="Home"><span>Home</span></a>
+                        <a href="admin-gallery.php"><img src="images/gallery.png" alt="Gallery"><span>Gallery</span></a>
+                        <a href="#"><img src="images/course-list.png" alt="Course List"><span>Course List</span></a>
+                        <a href="admin-alumni-list.php"><img src="images/alumni_list.png" alt="Alumni List"><span>Alumni List</span></a>
+                        <a href="#"><img src="images/upload.png" alt="Alumni Upload"><span>Alumni Upload</span></a>
+                        <a href="#"><img src="images/jobs.png" alt="Jobs"><span>Jobs</span></a>
+                        <a href="register.php"> <img src="images/calendar.png" alt="Events"><span>Events</span>
+                        <a href="landing.php"><img src="images/forums.png" alt="Forum"><span>Forum</span></a>
+                        <a href="admin-system-setting.php"><img src="images/settings.png" alt="System Settings"><span>System Settings</span></a>
+                        <a href="landing.php"><img src="images/log-out.png" alt="Log Out"><span>Log Out</span></a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="main-content">
+                <header>Upload</header>
+                <hr><br>
+                <div class="upload-form">
+                    <form class="form-upload">
+                        <input type="file" name="file">
+                        <br><br>
+                        <label>Short Description</label><br>
+                        <textarea></textarea>
+                        <button type="submit">Save</button>
+                        <button type="button">Cancel</button>
+
+                    </form>
+
+                </div>
+            </div>
+
+        </div>
+
+        
+
+      
+
+        <script>
+            function toggleSidebar() {
+                const sidebar = document.getElementById('sidebar');
+                sidebar.classList.toggle('collapsed');
+                const toggleBtn = document.querySelector('.toggle-btn');
+                toggleBtn.innerHTML = sidebar.classList.contains('collapsed') ? '&#x25B6;' : '&#x25C0;';
+            }
+        </script>
+
+
+        
+
+
+    </body>
+</html>
